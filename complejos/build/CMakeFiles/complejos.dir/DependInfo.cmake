673de575e73e0572833c6a8152b312e81eeefa0c
joseantonio002 <joseantunezpul@gmail.com>
2021-01-07 15:43:38 +0000

    make

---
+++ complejos/build/CMakeFiles/complejos.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/usuario/ib-practica11-oop-cmake-joseantonio002/complejos/src/complejos.cc" "/home/usuario/ib-practica11-oop-cmake-joseantonio002/complejos/build/CMakeFiles/complejos.dir/src/complejos.cc.o"
+  "/home/usuario/ib-practica11-oop-cmake-joseantonio002/complejos/src/main_complejos.cc" "/home/usuario/ib-practica11-oop-cmake-joseantonio002/complejos/build/CMakeFiles/complejos.dir/src/main_complejos.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ complejos/build/CMakeFiles/complejos.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/complejos.dir/src/complejos.cc.o"
+  "CMakeFiles/complejos.dir/src/main_complejos.cc.o"
+  "complejos.pdb"
+  "complejos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/complejos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
