5674053f27f236fc970d6e58408e248bef1fa0a9
joseantonio002 <joseantunezpul@gmail.com>
2021-01-07 15:49:22 +0000

    make

---
+++ racionales/build/CMakeFiles/racionales.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/usuario/ib-practica11-oop-cmake-joseantonio002/racionales/src/main_racionales.cc" "/home/usuario/ib-practica11-oop-cmake-joseantonio002/racionales/build/CMakeFiles/racionales.dir/src/main_racionales.cc.o"
+  "/home/usuario/ib-practica11-oop-cmake-joseantonio002/racionales/src/racionales.cc" "/home/usuario/ib-practica11-oop-cmake-joseantonio002/racionales/build/CMakeFiles/racionales.dir/src/racionales.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ racionales/build/CMakeFiles/racionales.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/racionales.dir/src/racionales.cc.o"
+  "CMakeFiles/racionales.dir/src/main_racionales.cc.o"
+  "racionales.pdb"
+  "racionales"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/racionales.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
