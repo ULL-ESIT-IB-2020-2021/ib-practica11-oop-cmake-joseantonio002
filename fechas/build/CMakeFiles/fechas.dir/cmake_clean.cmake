c2aef5490f0474de5544dfd26fb93d0d3ba18e44
joseantonio002 <joseantunezpul@gmail.com>
2021-01-07 15:48:31 +0000

    make

---
+++ fechas/build/CMakeFiles/fechas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fechas.dir/src/Fecha.cc.o"
+  "CMakeFiles/fechas.dir/src/main_fechas.cc.o"
+  "CMakeFiles/fechas.dir/src/func_fechas.cc.o"
+  "fechas.pdb"
+  "fechas"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fechas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
